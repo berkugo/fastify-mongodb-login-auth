924c2aa21bbee94e66229826475c13ce161e034c
ramazanhoyladi <ramazan@hoyladi.net>
2021-04-19 02:10:47 +0300

    Houses
    Ev Sistemi Endpointleri

---
+++ models/house.js
@@ -0,0 +1,41 @@
+// HOYLRAM 19.04.2021
+const mongoose = require('mongoose');
+
+const houseSchema = new mongoose.Schema({
+    _id: {type: String},
+    name: {type: String, required: true},
+    originalName: {type: String},
+    owner: {type: String, default: null},
+    onsale: {type: Boolean, default: true},
+    price: {type: Number, required: true},
+    originalPrice: {type: Number},
+    locked: {type: Boolean, default: true},
+    store: [],
+    personel: [],
+    input: {
+        x: {type: Number, default: 0},
+        y: {type: Number, default: 0},
+        z: {type: Number, default: 0},
+        d: {type: Number, default: 0}
+    },
+    output: {
+        x: {type: Number, default: 0},
+        y: {type: Number, default: 0},
+        z: {type: Number, default: 0},
+        d: {type: Number, default: 0}
+    }
+})
+
+houseSchema.pre('save', function(next){
+    if(!this.originalName){
+        this.originalName = this.name
+    }
+    if(!this.originalPrice){
+        this.originalPrice = this.price
+    }
+    next()
+})
+
+const houseModel = new mongoose.model('houses', houseSchema)
+
+module.exports = houseModel
---
+++ routes/house.js
@@ -0,0 +1,46 @@
+// HOYLRAM 19.04.2021
+const houseModel = require("../models/house")
+const router = async (fastify, options, done) => {
+
+    fastify.post("/create", options, async (req, res) => {
+        const { name, price, input } = req.body
+        if (name && price && input) {
+            const instance = new houseModel({
+                name: name,
+                price: price,
+                input: input
+            })
+            instance.save()
+            return res.send({ result: true })
+        } else {
+            return res.code(404).send({ result: { message: "Already created or required params are lack of." } })
+        }
+    })
+
+    fastify.post("/match", options, async (req, res) => {
+        const isExist = await houseModel.exists({ _id: req.body._id })
+        if (isExist && req.body._id) {
+            const data = await houseModel.findOne({ _id: req.body._id }).exec()
+            return res.code(200).send(data)
+        }
+        else {
+            return res.code(404).send({ result: { message: "Error." } })
+        }
+    })
+
+    fastify.post("/update", options, async (req, res) => {
+        const isExist = await houseModel.exists({ _id: req.body._id })
+        if (isExist) {
+            const data = await houseModel.findOneAndUpdate({ _id: req.body._id }, req.body.data, {
+                new: true,
+            });
+            return res.code(200).send(data)
+        }
+        else {
+            return res.code(404).send({ result: { message: "Error." } })
+        }
+    })
+    
+    done()
+}
+module.exports = router
