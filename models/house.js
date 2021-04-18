12d7234f4517a486c37f7afa71230378135e2425
ramazanhoyladi <ramazan@hoyladi.net>
2021-04-19 02:34:23 +0300

    düzeltme 2

--- models/house.js
+++ models/house.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 
 const houseSchema = new mongoose.Schema({
-    _id: {type: String},
     name: {type: String, required: true},
     originalName: {type: String},
     owner: {type: String, default: null},
