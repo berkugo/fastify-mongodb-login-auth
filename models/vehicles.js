e60c390c625ac871f3f6787f0844f6705785762f
berk <berk.ugo@gmail.com>
2021-04-23 23:25:53 +0300

    vehiclesdata

--- models/vehicles.js
+++ models/vehicles.js
@@ -4,7 +4,18 @@ const vehicleSchema = new mongoose.Schema({
     ownerId: { type: String, required: true },
     position: { type: Object, default: {} },
     primaryColor: { type: Object, default: {} },
-    secondaryColor: { type: Object, default: {} }
+    secondaryColor: { type: Object, default: {} },
+    plate: { type: String, required: true },
+    insurance: { type: Number, default: 30 },
+    penalties: { type: Array, default: [] },
+    createdDate: { type: Number, default: Date.now },
+    keys: {
+        type: Object, default: {
+            1: null,
+            2: null,
+            3: null
+        }
+    }
 })
 
 const vehicleModel = new mongoose.model('vehicles', vehicleSchema)
