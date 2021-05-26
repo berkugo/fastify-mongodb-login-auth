788d6ac07edae29d8164f8c8057766d9c0837254
baran <dasdemirbaran@gmail.com>
2021-05-26 17:09:09 +0300

    update db config

--- db/db.js
+++ db/db.js
@@ -1,17 +1,14 @@
-
 const mongoose = require('mongoose');
-const url = "mongodb://xrpadmin:chr123321@altv.turnuvam.net:27017/xrp"
+const url = 'mongodb://xrpadmin:%5Exxejc%23M9k%40Qt63%214%21%2549%25E6y%269QM%24Q4@altv.turnuvam.net:27017/xrp?authSource=admin&authMechanism=SCRAM-SHA-256';
 const autoIncrement = require('mongoose-auto-increment');
 
-function createConnectionToDb(){
-    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function () {
-    });
-    autoIncrement.initialize(db);
-
+function createConnectionToDb() {
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {});
+  autoIncrement.initialize(db);
 }
 createConnectionToDb();
 
-module.exports = createConnectionToDb
+module.exports = createConnectionToDb;
--- routes/players.js
+++ routes/players.js
@@ -1,80 +1,47 @@
-const userModel = require("../models/users")
+const userModel = require('../models/users');
 
 const router = async (fastify, options, done) => {
+  fastify.post('/create', options, async (req, reply) => {
+    const alreadyExist = await userModel.exists({ userName: req.body.userName });
+    if (req.body.userName && req.body.userPass && req.body.characterName && !alreadyExist) {
+      const instance = new userModel({
+        userName: req.body.userName,
+        userPassword: req.body.userPass,
+        characterName: req.body.characterName,
+      });
+      instance.save();
+      return reply.send({ result: true });
+    } else {
+      return reply
+        .code(404)
+        .send({ result: { message: 'Already created or required params are lack of.' } });
+    }
+  });
 
-    fastify.post("/create", options, async (req, reply) => {
-        const alreadyExist = await userModel.exists({ userName: req.body.userName })
-        if (req.body.userName && req.body.userPass && req.body.characterName && !alreadyExist) {
+  fastify.post('/match', options, async (req, res) => {
+    const playerExist = await userModel.exists({ userName: req.body.userName });
+    if (playerExist && req.body.userName && req.body.userPass) {
+      const playerData = await userModel
+        .findOne({ userName: req.body.userName, userPassword: req.body.userPass })
+        .exec();
+      return res.code(200).send(playerData);
+    } else {
+      return res.code(404).send({ result: { message: 'Error.' } });
+    }
+  });
 
-            const instance = new userModel({
-                userName: req.body.userName, userPassword: req.body.userPass,
-                characterName: req.body.characterName
-            })
-            instance.save()
-            return reply.send({ result: true })
+  fastify.post('/update', options, async (req, res) => {
+    const playerExist = await userModel.exists({ _id: req.body.uuid });
+    if (playerExist) {
+      const data = await userModel.findOneAndUpdate({ _id: req.body.uuid }, req.body.data, {
+        new: true,
+      });
+      return res.code(200).send(data);
+    } else {
+      return res.code(404).send({ result: { message: 'Error.' } });
+    }
+  });
 
-        } else {
-
-            return reply.code(404).send({ result: { message: "Already created or required params are lack of." } })
-        }
-
-    })
-
-    fastify.post("/match", options, async (req, res) => {
-
-        const playerExist = await userModel.exists({ userName: req.body.userName })
-        if (playerExist && req.body.userName && req.body.userPass) {
-            const playerData = await userModel.findOne({ userName: req.body.userName, userPassword: req.body.userPass }).exec()
-            return res.code(200).send(playerData)
-        }
-        else {
-            return res.code(404).send({ result: { message: "Error." } })
-        }
-
-    })
-
-    fastify.post("/filter", options, async (req, res) => {
-
-        const playerExist = await userModel.exists((JSON.parse(JSON.stringify(req.body))));
-        if (playerExist) {
-            const playerData = await userModel.find((JSON.parse(JSON.stringify(req.body)))).exec()
-            return res.code(200).send(playerData)
-        }
-        else {
-            return res.code(404).send({ result: { message: "Error." } })
-        }
-
-    })
-
-    fastify.post("/update", options, async (req, res) => {
-        if (req.body.uuid && (req.body.uuid.length < 5) || req.body.uuid === 0) {
-            const playerExist = await userModel.exists({ xid: req.body.uuid })
-            if (playerExist) {
-                const data = await userModel.findOneAndUpdate({ xid: req.body.uuid }, req.body.data, {
-                    new: true,
-                });
-                return res.code(200).send(data)
-            } else {
-                return res.code(404).send({ result: { message: "Error." } })
-            }
-        } else {
-            const playerExist = await userModel.exists({ _id: req.body.uuid })
-            if (playerExist) {
-                const data = await userModel.findOneAndUpdate({ _id: req.body.uuid }, req.body.data, {
-                    new: true,
-                });
-                return res.code(200).send(data)
-            }
-            else {
-                return res.code(404).send({ result: { message: "Error." } })
-            }
-        }
-    })
-
-    done()
-}
-module.exports = router
-
-
-
-
+  done();
+};
+module.exports = router;
